aadf730c60899bd325f2618d03ae8e2a712b6c8b
Valeria Kliui <valeria14003@gmail.com>
2022-11-24 12:33:46 +0300

    refactor desktop version of main page

--- pages/main/script.js
+++ pages/main/script.js
@@ -12,7 +12,7 @@ let testimonials = document.querySelectorAll('.testimonial-text__container');
 for (let testimonial of testimonials) {
 let testimonialText = testimonial.innerHTML;
 let slicedText = testimonialText.slice(0,1200);
-if (windowInnerWidth === 320) {
+if (windowInnerWidth<=320) {
     slicedText = testimonialText.slice(0,190);
 }
 if (slicedText.length<testimonialText.length){
@@ -22,9 +22,38 @@ if (slicedText.length<testimonialText.length){
 
 //hiding of link "donate" in footer
 let linksNavigation = document.querySelectorAll('.navigation__item-footer');
-console.log(linksNavigation)
 for (let link of linksNavigation) {
     if (link.textContent.trim() === 'Donate') {
         link.hidden=true;
     } 
 }
+
+//burger and menu-mobile
+let burger = document.querySelector('.burger');
+
+let menuNav = document.querySelector('.navigation-desktop');
+let menuList = document.querySelector('.navigation__list');
+let header = document.querySelector('.header');
+let logo = document.querySelector('.logo__container');
+menuNav.classList.add('hidden');
+
+burger.onclick =()=>{
+burger.classList.toggle('close');
+
+header.append(menuNav);
+menuNav.classList.toggle('hidden');
+menuNav.classList.toggle('menu__mobile');
+menuList.classList.toggle('navigation__list-mobile');   
+}
+
+//hide arrows on small display
+let arrowLeft = document.querySelector('.arrow-left');
+let arrowRight = document.querySelector('.arrow-right');
+window.onresize = function (e) {console.log(e.target.outerWidth, e.target.outerHeight)
+if (window.innerWidth>1000 && window.innerWidth<=1280) {
+    arrowLeft.hidden=true;
+    arrowRight.hidden=true;
+}
+}
+
+
