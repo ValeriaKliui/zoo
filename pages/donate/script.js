4d3f61f74741834f135b00a6c9a3df3eb4ab52c6
Valeria Kliui <valeria14003@gmail.com>
2022-11-27 21:23:18 +0300

    git change range

--- pages/donate/script.js
+++ pages/donate/script.js
@@ -15,8 +15,8 @@ for (let link of links) {
 let linksNavigation = document.querySelectorAll('.navigation__item-footer');
 for (let link of linksNavigation) {
     if (link.textContent.trim() === 'Donate') {
-        link.hidden=true;
-    } 
+        link.hidden = true;
+    }
 }
 
 //burger and menu-mobile
@@ -28,33 +28,33 @@ let header = document.querySelector('.header');
 let logo = document.querySelector('.logo__container');
 menuNav.classList.add('hidden');
 
-burger.onclick =()=>{
-burger.classList.toggle('close');
+burger.onclick = () => {
+    burger.classList.toggle('close');
 
-header.append(menuNav);
-menuNav.classList.toggle('hidden');
-menuNav.classList.toggle('menu__mobile');
-menuList.classList.toggle('navigation__list-mobile');   
+    header.append(menuNav);
+    menuNav.classList.toggle('hidden');
+    menuNav.classList.toggle('menu__mobile');
+    menuList.classList.toggle('navigation__list-mobile');
 }
 
 //range values
 let range = document.querySelector('.scrol');
 let textValues = document.querySelectorAll('.range__value');
-window.onresize = resizeWindow();
-
-function resizeWindow (){
-    if (windowInnerWidth >tabletScreen && windowInnerWidth< smallDesktop) {
+window.onresize = ()=> resizeWindow() ;
+resizeWindow();
+function resizeWindow() {
+    console.log('k')
+    if (windowInnerWidth > tabletScreen && windowInnerWidth < smallDesktop) {
         range.min = 25;
         range.max = 175;
-        }
-        if (windowInnerWidth< tabletScreen) {
-            range.min = 25;
-            range.max = 125;
-            }
-        for (let textValue of textValues) {
-            if ( !(+textValue.textContent.slice(1) >= +range.min)  || !(+textValue.textContent.slice(1) <= +range.max))
-        {
+    }
+    if (windowInnerWidth < tabletScreen) {
+        range.min = 25;
+        range.max = 125;
+    }
+    for (let textValue of textValues) {
+        if (!(+textValue.textContent.slice(1) >= +range.min) || !(+textValue.textContent.slice(1) <= +range.max)) {
             textValue.remove();
         }
-}
+    }
 }
--- pages/main/script.js
+++ pages/main/script.js
@@ -14,60 +14,50 @@ for (let link of links) {
 
 //cropping of testimonials 
 let testimonials = document.querySelectorAll('.testimonial-text__container');
-for (let testimonial of testimonials) {
-let testimonialText = testimonial.innerHTML;
-let slicedText = testimonialText.slice(0,1200);
-if (windowInnerWidth<=mobileScreen) {
-    slicedText = testimonialText.slice(0,190);
+cropTestimonials();
+window.onresize = ()=> cropTestimonials();
+
+function cropTestimonials() {
+    for (let testimonial of testimonials) {
+        let testimonialText = testimonial.innerHTML;
+        let slicedText = testimonialText.slice(0, 1200);
+        if (windowInnerWidth <= mobileScreen) {
+            slicedText = testimonialText.slice(0, 190);
+        }
+        else if (windowInnerWidth > mobileScreen && windowInnerWidth < tabletScreen) {
+            slicedText = testimonialText.slice(0, 250);
+        }
+        else if (windowInnerWidth >= tabletScreen && windowInnerWidth <= smallDesktop) {
+            slicedText = testimonialText.slice(0, 800);
+        }
+        if (slicedText.length < testimonialText.length) {
+            testimonial.innerHTML = slicedText.trim() + '...';
+        }
+    }
 }
-if (windowInnerWidth>mobileScreen && windowInnerWidth<tabletScreen) {
-    slicedText = testimonialText.slice(0,250);
-}
-if (windowInnerWidth>=tabletScreen && windowInnerWidth<=smallDesktop) {
-    slicedText = testimonialText.slice(0,800);
-}
-if (slicedText.length<testimonialText.length){
-    testimonial.innerHTML = slicedText.trim() + '...';
-}
-}
+
 
 //hiding of link "donate" in footer
 let linksNavigation = document.querySelectorAll('.navigation__item-footer');
 for (let link of linksNavigation) {
     if (link.textContent.trim() === 'Donate') {
-        link.hidden=true;
-    } 
+        link.hidden = true;
+    }
 }
 
 //burger and menu-mobile
 let burger = document.querySelector('.burger');
-
+let area = document.querySelector('body');
 let menuNav = document.querySelector('.navigation-desktop');
 let menuList = document.querySelector('.navigation__list');
 let header = document.querySelector('.header');
 let logo = document.querySelector('.logo__container');
 menuNav.classList.add('hidden');
 
-burger.onclick =()=>{
-burger.classList.toggle('close');
-
-header.append(menuNav);
-menuNav.classList.toggle('hidden');
-menuNav.classList.toggle('menu__mobile');
-menuList.classList.toggle('navigation__list-mobile');   
+burger.onclick = () => {
+    burger.classList.toggle('close');
+    header.append(menuNav);
+    menuNav.classList.toggle('hidden');
+    menuNav.classList.toggle('menu__mobile');
+    menuList.classList.toggle('navigation__list-mobile');
 }
-
-//hide arrows on small display
-let arrowLeft = document.querySelector('.arrow-left');
-let arrowRight = document.querySelector('.arrow-right');
-window.onresize = function () {
-if (window.innerWidth>smallDesktop && window.innerWidth<=1280) {
-    arrowLeft.hidden=true;
-    arrowRight.hidden=true;
-}
-else     {arrowLeft.hidden=false;
-arrowRight.hidden=false;
-}
-}
-
-
